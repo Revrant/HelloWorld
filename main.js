415eb35144a85b15643be07ffe13a3ba23ac6d9f
Revrant <jayhawktristen@hotmail.com>
2017-06-03 00:13:24 -0500

    Add delete button

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-function addItem()
-{
-	var add = document.createElement('li');
-	add.innerHTML = text.value;
-
-	if(text.value == ''){
-		alert("You need type something!");
-	}
-	else{
-		para.appendChild(add);
-		text.value = '';
-	}
-}
-
+var size = 0;
 
 var element = document.getElementById('root');
 var title = document.createElement('div');
@@ -20,15 +7,52 @@ title.innerHTML = 'List:';
 var text = document.createElement("input"); 
 text.type = "text";
 
+var id = document.createElement("id");
+
 var click = document.createElement('onclick');
 var button = document.createElement("button");
 button.innerHTML = "Add to List";
 button.onclick = function() {addItem()};
 
-var para = document.createElement('ul');
-para.className = "unlist";
+var list = document.createElement('ul');
+list.className = "unlist";
+
+var addItem = function()
+{
+	if(text.value == ''){
+		alert("You need type something!");
+	}
+	else{
+		var titlediv = document.createElement('div');
+		var add = document.createElement('li');
+		var close = document.createElement('div');
+
+		titlediv.innerHTML = text.value;
+		add.id = size + '';
+	
+		close.innerHTML = "x";
+		close.style.color = 'red';
+		close.onclick = function() {deleteItem(add.id)};
+		
+
+		close.className = "deletebutton";
+		titlediv.className = "titlediv";
+		size++;
+		add.appendChild(titlediv);
+		add.appendChild(close);
+		list.appendChild(add);
+		text.value = '';
+	}
+}
+
+var deleteItem = function(id)
+{
+	var temp = document.getElementById(id);
+
+	temp.parentNode.removeChild(temp);
+}
 
 element.appendChild(button);
 element.appendChild(text);
 element.appendChild(title);
-element.appendChild(para);
+element.appendChild(list);
