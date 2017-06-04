1b8fddc68e2aff64d56d43fc803230118c7c27e5
Revrant <jayhawktristen@hotmail.com>
2017-06-04 16:27:31 -0500

    Add timestamp

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var size = 0;
++new Date();
 
 var element = document.getElementById('root');
 var title = document.createElement('div');
@@ -6,6 +7,7 @@ title.innerHTML = 'List:';
 
 var text = document.createElement("input"); 
 text.type = "text";
+text.className = "input";
 
 var id = document.createElement("id");
 
@@ -13,22 +15,36 @@ var click = document.createElement('onclick');
 var button = document.createElement("button");
 button.innerHTML = "Add to List";
 button.onclick = function() {addItem()};
+button.className = "pressbutton";
 
 var list = document.createElement('ul');
 list.className = "unlist";
 
 var addItem = function()
 {
-	if(text.value == ''){
+	if(text.value === ''){
 		alert("You need type something!");
 	}
 	else{
 		var titlediv = document.createElement('div');
 		var add = document.createElement('li');
 		var close = document.createElement('div');
+		var date = document.createElement('datetime');
+		var currdate = new Date(Date.now());
+
+		date.className = 'datetime';
+		if(currdate.getMinutes() >= 10){
+			date.innerHTML = (currdate.getMonth() - 1) + "/" + currdate.getDate() + "/" + currdate.getFullYear() + "   " + currdate.getHours() + ":" + currdate.getMinutes();
+		}
+		else {
+			date.innerHTML = (currdate.getMonth() - 1) + "/" + currdate.getDate() + "/" + currdate.getFullYear() + "   " + currdate.getHours() + ":" + "0" + currdate.getMinutes();
+
+		}	
+
 
 		titlediv.innerHTML = text.value;
 		add.id = size + '';
+
 	
 		close.innerHTML = "x";
 		close.style.color = 'red';
@@ -40,6 +56,7 @@ var addItem = function()
 		size++;
 		add.appendChild(titlediv);
 		add.appendChild(close);
+		add.appendChild(date);
 		list.appendChild(add);
 		text.value = '';
 	}
