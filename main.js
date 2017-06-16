f841d70b311ae7af82a1db906149ee7008cd7da5
Revrant <jayhawktristen@hotmail.com>
2017-06-16 14:33:13 -0500

    add style

--- main.js
+++ main.js
@@ -1,32 +1,58 @@
 var size = 0;
-+new Date();
+var clicked = false;
+var data = [
+	{
+		input: "Go get milk",
+		timestamp: "12/7/2017 : 5:32"
+	},
+	{
+		input: "Go to store",
+		timestamp: "1/13/2018 : 12:00"
+	}
+];
 
+
++new Date();
 var element = document.getElementById('root');
 var title = document.createElement('div');
 var header = document.createElement('div');
-title.innerHTML = 'My To Do List:';
-title.className = 'title';
-header.className = 'header';
-
+var datalist = document.createElement('li');
 var text = document.createElement("input"); 
-text.type = "text";
-text.className = "input";
-
 var id = document.createElement("id");
-
 var click = document.createElement('onclick');
 var button = document.createElement("button");
+var list = document.createElement('ul');
+
+title.innerHTML = 'To Do List:';
+title.className = 'title';
+header.className = 'header';
+text.type = "text";
+text.className = "input";
 button.innerHTML = "Add to List";
 button.onclick = function() {addItem()};
 button.className = "pressbutton";
-
-var list = document.createElement('ul');
 list.className = "unlist";
 
+/*for(var i = 0; i < data.length; i++)
+{
+	var temp = document.getElementById('li');
+	temp.innerHTML = data[i].input;
+	datalist.appendChild(temp);
+	temp.innerHTML = data[i].timestamp;
+	datalist.appendChild(temp);
+}*/
+
+list.appendChild(datalist);
 header.appendChild(title);
 header.appendChild(text);
 header.appendChild(button);
 
+var metaTag=document.createElement('meta');
+metaTag.name = "viewport"
+metaTag.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+document.getElementsByTagName('head')[0].appendChild(metaTag);
+viewport = document.querySelector("meta[name=viewport]");
+viewport.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0');
 
 
 var addItem = function()
@@ -55,7 +81,7 @@ var addItem = function()
 		add.id = size + '';
 
 		close.innerHTML = "x";
-		close.style.color = 'red';
+		close.style.color = 'grey';
 		close.onclick = function() {deleteItem(add.id)};
 		
 		close.className = "deletebutton";
